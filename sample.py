163dda9dea4cd8a394eb3fa7b982a9cebefaa4ed
Morteza <maleki.morteza92@gmail.com>
2024-07-02 21:49:59 +0330

    added sprite

--- sample.py
+++ sample.py
@@ -10,32 +10,85 @@ pygame.init()
 SCREEN_WIDTH = 500
 SCREEN_HEIGHT = 500
 
+
+
 dino = PygameImageArray(tile_size=(140, 140), sprite_sheet_path='AMBULANCE_CLEAN_ALLD0000-sheet.png', scale=0.5)
 dino.plot_it()
-right_down = AnimArray(dino[0:2, :], scale=0.5, reverse=(False, False))
 
-up_right = AnimArray(np.array(list(dino[5]) + list(dino[6])[:-1]), scale=0.5, reverse=(False, False)) 
-go_up = AnimArray(dino[5, :2], scale=0.5, reverse=(False, False))
-go_right = AnimArray(dino[0, :2], scale=0.5, reverse=(False, False))
-go_left = AnimArray(dino[0, :2], scale=0.5, reverse=(True, Tru))
-go_fast = AnimArray(dino[0, :], scale=0.5, reverse=(False, False))
-go_down = AnimArray(dino[1, 4:6], scale=0.5, reverse=(False, False))
-right_up = AnimArray(np.array(list(dino[5]) + list(dino[6])[:-1])[::-1], scale=0.5, reverse=(False, False))
+
+scale = 2
+right_down = AnimArray(dino[0:2, :], scale=scale, reverse=(False, False))
+up_right = AnimArray(np.array(list(dino[5]) + list(dino[6])[:-1]), scale=scale, reverse=(False, False)) 
+go_up = AnimArray(dino[5, :2], scale=scale, reverse=(False, False))
+go_right = AnimArray(dino[0, :2], scale=scale, reverse=(False, False))
+go_left = AnimArray(dino[0, :2], scale=scale, reverse=(True, False))
+go_fast = AnimArray(dino[0, :], scale=scale, reverse=(False, False))
+go_down = AnimArray(dino[1, 4:6], scale=scale, reverse=(False, False))
+right_up = AnimArray(np.array(list(dino[5]) + list(dino[6])[:-1])[::-1], scale=scale, reverse=(False, False))
 
 all_anims = {"R": go_right,
-			 "L": go_left,
-			 "Fast": go_fast,
-			 "D": go_down,
-			 "U": go_up,
-			 "R-D": right_down,
-			 "U-R":up_right,
-			 "R-U": right_up,
-			 "default": go_right}
+             "L": go_left,
+             "Fast": go_fast,
+             "D": go_down,
+             "U": go_up,
+             "R-D": right_down,
+             "D-R": right_down.reverse_anim_array(),
+             "U-R":up_right,
+             "R-U": right_up,
+             "default": go_right
+             }
 
 frame_manager = FrameManager(all_anims)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Spritesheets')
 
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+        
+        self.image = frame_manager.get_frame()
+
+        self.rect = self.image.get_rect()
+        self.rect.centerx = SCREEN_WIDTH / 2
+        self.rect.bottom = SCREEN_HEIGHT - 10
+        self.speedx = 20
+        self.speedy = 20
+
+    def update(self):
+        # self.speedx = 0
+        # keystate = pygame.key.get_pressed()
+        # if keystate[pygame.K_LEFT]:
+        #     self.speedx = -8
+        # if keystate[pygame.K_RIGHT]:
+        #     self.speedx = 8
+        # self.rect.x += self.speedx
+        # if self.rect.right > SCREEN_WIDTH:
+        #     self.rect.right = SCREEN_WIDTH
+        # if self.rect.left < 0:
+        #     self.rect.left = 0
+        self.image = frame_manager.get_frame()
+
+        key = pygame.key.get_pressed()
+        if key[pygame.K_DOWN]:
+            self.rect.y += self.speedy
+            frame_manager.add_anim_state("D")
+        if key[pygame.K_RIGHT]:
+            self.rect.x += self.speedx
+            frame_manager.add_anim_state("R")
+        if key[pygame.K_UP]:
+            self.rect.y -= self.speedy 
+            frame_manager.add_anim_state("U")
+        if key[pygame.K_LEFT]:
+            self.rect.x -= self.speedx
+            frame_manager.add_anim_state("L")
+                
+
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 # frame_manager.set_default_anim(go_right)
 BG = (50, 50, 50)
 BLACK = (0, 0, 0, 0)
@@ -46,34 +99,18 @@ x, y = 0, 0
 
 run = True
 while run:
-	screen.fill(BG)
-	
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			run = False
+    screen.fill(BG)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
-	key = pygame.key.get_pressed()
-	# if key[pygame.K_RIGHT]:
-	# 	x += 5
-	# 	screen.blit(go_right.get_sprtie(), (x, y))
-	# if key[pygame.K_t]:
-	# 	screen.blit(go_right.get_sprtie(pre_transition=["turn_right_from_right"]), (x, y))
 
-	if key[pygame.K_DOWN]:
-		y += 2
-		frame_manager.add_anim_state("D")
-	if key[pygame.K_RIGHT]:
-		x += 2
-		frame_manager.add_anim_state("R")
-	if key[pygame.K_UP]:
-		y -= 2 
-		frame_manager.add_anim_state("U")
-	if key[pygame.K_LEFT]:
-		x -= 2
-		frame_manager.add_anim_state("L")
-	screen.blit(frame_manager.get_frame(), (x, y))
+    # screen.blit(frame_manager.get_frame(), (x, y))
+    all_sprites.update()
+    all_sprites.draw(screen)
 
-	pygame.display.update()
-	clock.tick(10)
+    pygame.display.update()
+    clock.tick(20)
 
 pygame.quit()
