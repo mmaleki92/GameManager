17c2beb950e718a3c8da72d84469b5897391ce91
Morteza Maleki <74877833+mmaleki92@users.noreply.github.com>
2024-07-19 22:00:03 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='game_manager',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     install_requires=["numpy", "natsort", "pygame"],
     entry_points={
