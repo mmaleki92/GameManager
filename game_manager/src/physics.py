5da585314d324bc1dbdb4c11794fb6c8b99a591d
Morteza <maleki.morteza92@gmail.com>
2024-07-25 18:55:01 +0330

    cleaning

--- game_manager/src/physics.py
+++ game_manager/src/physics.py
@@ -4,8 +4,9 @@ class Physics:
         self.wind = wind
         self.velocity_x = 0
         self.velocity_y = 0
-    
+
     def apply_forces(self):
         self.velocity_y += self.gravity
         self.velocity_x += self.wind
         return self.velocity_x, self.velocity_y    
+
