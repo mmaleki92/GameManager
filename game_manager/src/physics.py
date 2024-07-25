eba53ecc8e8ab93d21aa0e099bf6a4d106202936
Morteza <maleki.morteza92@gmail.com>
2024-07-25 18:33:26 +0330

    added physics

---
+++ game_manager/src/physics.py
@@ -0,0 +1,11 @@
+class Physics:
+    def __init__(self, gravity=0.5, wind=0.0):
+        self.gravity = gravity
+        self.wind = wind
+        self.velocity_x = 0
+        self.velocity_y = 0
+    
+    def apply_forces(self):
+        self.velocity_y += self.gravity
+        self.velocity_x += self.wind
+        return self.velocity_x, self.velocity_y    
