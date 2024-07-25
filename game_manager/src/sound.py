ed98f65c54ee271c0043018d2a78ae279847d7c0
Morteza <maleki.morteza92@gmail.com>
2024-07-25 20:21:07 +0330

    added a sound manager

---
+++ game_manager/src/sound.py
@@ -0,0 +1,28 @@
+import pygame
+
+
+class Sound:
+    def __init__(self, path) -> None:
+        self.sound = pygame.mixer.Sound(path)
+    
+    def play(self):
+        self.sound.play(loops = -1)
+
+    def stop(self):
+        self.sound.stop()
+
+
+class SoundManager:
+    def __init__(self) -> None:
+        self.sound_dict = {}
+
+    def add_sound_from_directory(self):
+        raise NotImplementedError
+
+    def add_sound_from_path(self, sound_name: str, path):
+        self.sound_dict[sound_name] = Sound(path)
+
+    def play_by_name(self, name):
+        sound = self.sound_dict.get(name, None) 
+        if sound:
+            sound.play()
