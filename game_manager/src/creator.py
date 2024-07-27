33d627099fcdc07e4a5180b63b28af3862ee94a1
Morteza <maleki.morteza92@gmail.com>
2024-07-27 18:47:17 +0330

    added a generic ui

--- game_manager/src/creator.py
+++ game_manager/src/creator.py
@@ -1,6 +1,7 @@
 import pygame
 from collections import deque
 from .sprite_sheet_array import PygameImageArray
+from .ui import UI
 
 class AnimCreator:
     def __init__(self, pygame_image_array_obj: PygameImageArray, scale=1) -> None:
@@ -17,6 +18,8 @@ class AnimCreator:
         self.sidebar_line = [((self.cols * self.tile_size[0]) * self.scale, 0),
                              ((self.cols * self.tile_size[0]) * self.scale, self.screen_height)]
     
+    def fun(self):
+        print("Hi from fun!")
 
     def run_pygame_display(self):
         # Initialize Pygame
@@ -27,18 +30,22 @@ class AnimCreator:
         clock = pygame.time.Clock()
         selected_sprites = deque()
 
-        # Font for displaying text in caption
-        # font = pygame.font.SysFont('Arial', 16)
-        
+        self.ui = UI(screen)
+
+        self.ui.add_button("btn1", (300, 200), (100, 50), "click me!", "<b>Click to Start.</b>")
+        self.ui.bind_function("btn1", self.fun)
+
         running = True
         while running:
+            frame_time = clock.tick(60)
+            time_delta = min(frame_time/1000.0, 0.1)
 
             hovered_tile = self.get_hoverd_tile()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
                 
-                if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.type == pygame.MOUSEBUTTONDOWN and (self._images_array.shape[0] > hovered_tile[0] and self._images_array.shape[1] >  hovered_tile[1]):
                     tile_surface = pygame.transform.scale(self._images_array[(hovered_tile[0], hovered_tile[1])], (self.tile_size[0] * self.scale, self.tile_size[1] * self.scale))
                     selected_sprites.append(tile_surface)
                     # selected_sprites.extend([tile_surface]*5)
@@ -52,7 +59,10 @@ class AnimCreator:
 
                     if event.key == pygame.K_c:
                         selected_sprites.clear()
-                        
+
+                self.ui.run(event)
+
+
             screen.fill((0, 0, 0))
             pygame.draw.line(screen, (255, 0, 0), self.sidebar_line[0], self.sidebar_line[1], width=2)
 
@@ -68,6 +78,14 @@ class AnimCreator:
             if selected_sprites and run_anim:
                 for sprite_surface in self.infinite_sprite_generator(selected_sprites):
                     screen.blit(sprite_surface, ((self.cols) * self.tile_size[0] * self.scale, self.tile_size[1] * self.scale))
+            
+
+            self.ui.ui_manager.update(time_delta)
+
+            # surface.blit(self.background_image, (0, 0))  # draw the background
+
+            self.ui.ui_manager.draw_ui(screen)
+
 
             # Update window caption with index of hovered tile
             caption_text = f"Tile index: ({int(hovered_tile[0])}, {int(hovered_tile[1])})"
---
+++ game_manager/src/ui.py
@@ -0,0 +1,129 @@
+import copy
+from typing import Tuple
+import pygame
+import pygame_gui
+from pygame_gui.elements.ui_button import UIButton
+from pygame_gui.elements.ui_label import UILabel
+
+
+class AppStateManager:
+    def __init__(self):
+        self.states = {}
+        self.active_state = None
+
+    def register_state(self, state):
+        if state.name not in self.states:
+            self.states[state.name] = state
+
+    def run(self, event):
+        if self.active_state is not None:
+            self.active_state.run(event)
+
+            if self.active_state.time_to_transition:
+                self.active_state.time_to_transition = False
+                new_state_name = self.active_state.target_state_name
+                self.active_state.end()
+                outgoing_data_copy = copy.deepcopy(self.active_state.outgoing_transition_data)
+                self.active_state = self.states[new_state_name]
+                self.active_state.incoming_transition_data = outgoing_data_copy
+                self.active_state.start()
+
+            if self.active_state.time_to_quit_app:
+                return False
+
+        return True
+
+    def set_initial_state(self, name):
+        if name in self.states:
+            self.active_state = self.states[name]
+            self.active_state.start()
+
+class BaseAppState:
+    def __init__(self, name, target_state_name, state_manager):
+        self.name = name
+        self.target_state_name = target_state_name
+        self.outgoing_transition_data = {}
+        self.incoming_transition_data = {}
+        self.state_manager = state_manager
+        self.time_to_transition = False
+        self.time_to_quit_app = False
+
+        self.state_manager.register_state(self)
+
+    def set_target_state_name(self, target_name):
+        self.target_state_name = target_name
+
+    def trigger_transition(self):
+        self.time_to_transition = True
+
+    def start(self):
+        pass
+
+    def end(self):
+        pass
+
+    def run(self, event):
+        pass
+
+class ScreenData:
+    def __init__(self, hud_size, editor_hud_size, screen_size):
+        self.screen_size = screen_size
+        self.hud_dimensions = hud_size
+        self.editor_hud_dimensions = editor_hud_size
+        self.play_area = [screen_size[0], screen_size[1] - self.hud_dimensions[1]]
+
+    def set_editor_active(self):
+        self.play_area = [self.screen_size[0], self.screen_size[1] - self.editor_hud_dimensions[1]]
+
+    def set_editor_inactive(self):
+        self.play_area = [self.screen_size[0], self.screen_size[1] - self.hud_dimensions[1]]
+
+
+class UI(BaseAppState):
+    def __init__(self, screen):
+        state_manager = AppStateManager()
+
+        super().__init__('creator_ui', 'select_level', state_manager)
+
+        self.elements = {}
+        self.bind_dict = {}
+        screen_size = screen.get_size()
+        
+        self.ui_manager = pygame_gui.UIManager(screen_size, "data/ui_theme.json")
+        self.ui_manager.preload_fonts([{'name': 'fira_code', 'point_size': 10, 'style': 'bold'},
+                                {'name': 'fira_code', 'point_size': 10, 'style': 'regular'},
+                                {'name': 'fira_code', 'point_size': 14, 'style': 'bold'}])
+
+        state_manager.set_initial_state('creator_ui')
+
+    def add_button(self, name: str, position: Tuple, size: Tuple, text: str, hover_text: str):
+        button = UIButton(pygame.Rect(position, size), text, self.ui_manager, tool_tip_text=hover_text)
+        self.elements[name] = button
+
+    def bind_function(self, name: str, function):
+        if isinstance(self.bind_dict.get(name), list): 
+                self.bind_dict[name].append(function)
+        else:
+            self.bind_dict[name] = [function]
+
+    def unbind_function(self, name: str, function):
+        function_list = self.bind_dict.get(name)
+
+        if function_list:
+            if function in function_list:
+                function_list.remove(function)
+
+    def end(self):
+        for e in self.elements:
+            e.kill()
+       
+    
+    def run(self, event):
+        self.ui_manager.process_events(event)
+
+        if event.type == pygame_gui.UI_BUTTON_PRESSED:
+            for name, function in self.elements.items():
+                if event.ui_element == function:
+                    for fun in self.bind_dict[name]: 
+                        fun()
+                    
