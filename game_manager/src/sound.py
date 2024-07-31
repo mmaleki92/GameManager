f3def6231a954c6344b89bb0d59e876f67cf7361
Morteza <maleki.morteza92@gmail.com>
2024-08-01 00:44:52 +0330

    play once

--- game_manager/src/sound.py
+++ game_manager/src/sound.py
@@ -5,8 +5,12 @@ class Sound:
     def __init__(self, path) -> None:
         self.sound = pygame.mixer.Sound(path)
     
-    def play(self):
-        self.sound.play(loops = -1)
+    def play(self, play_once):
+        if play_once:
+            self.sound.play()
+        else:
+            self.sound.play(loops=-1)
+
 
     def stop(self):
         self.sound.stop()
@@ -22,7 +26,7 @@ class SoundManager:
     def add_sound_from_path(self, sound_name: str, path):
         self.sound_dict[sound_name] = Sound(path)
 
-    def play_by_name(self, name):
+    def play_by_name(self, name, play_once=False):
         sound = self.sound_dict.get(name, None) 
         if sound:
-            sound.play()
+            sound.play(play_once)
