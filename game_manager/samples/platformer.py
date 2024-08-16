66462ef9431351d2bebd38686352d2404d1674c6
Morteza <maleki.morteza92@gmail.com>
2024-08-16 15:41:27 +0330

    refactoring

--- game_manager/samples/platformer.py
+++ game_manager/samples/platformer.py
@@ -1,103 +1,20 @@
-"""Showcase of a very basic 2d platformer
-
-The red girl sprite is taken from Sithjester's RMXP Resources:
-http://untamed.wild-refuge.net/rmxpresources.php?characters
-
-.. note:: The code of this example is a bit messy. If you adapt this to your 
-    own code you might want to structure it a bit differently.
-"""
-
-__docformat__ = "reStructuredText"
 
 import math
 import sys
 import os.path
-import numpy as np
 import pygame
 
 import pymunk
 import pymunk.pygame_util
 from pymunk.vec2d import Vec2d
 from game_manager.src.image_to_mesh import add_sprite_mesh
-from rotate_image import blitRotate
-from game_manager.src.pymunk_shapes import moving_body
+from game_manager.src.image_rotate import blitRotate
+from game_manager.src.pymunk_shapes import (moving_body, create_passthrough_platform,
+                                            add_segments, move_platform_body,
+                                            handle_impulse, cpflerpconst, Grounding)
 from game_manager.src import sound
-from game_manager.src.sprite_sheet_array import PygameImageArray, AnimArray, FrameManager, SpriteText
-
-def cpfclamp(f, min_, max_):
-    """Clamp f between min and max"""
-    return min(max(f, min_), max_)
-
-
-def cpflerpconst(f1, f2, d):
-    """Linearly interpolate from f1 to f2 by no more than d."""
-    return f1 + cpfclamp(f2 - f1, -d, d)
-
-def flipy(y):
-    """Small hack to convert chipmunk physics to pygame coordinates"""
-    return -y + 600
-def handle_impulse(body, grounding, landed_previous, landing, sound_manager):
-    """
-    Handle the impulse when the player lands on the ground.
-    
-    Args:
-        body: The player's body object.
-        grounding: A dictionary containing information about the player's grounding.
-        landed_previous: A boolean indicating whether the player had landed in the previous frame.
-        landing: A dictionary containing the landing position and countdown for the landing effect.
-        sound_manager: The sound manager object to play sound effects.
-    
-    Returns:
-        A tuple of (landed_previous, landing).
-    """
-    if abs(grounding["impulse"].y) / body.mass > 200 and not landed_previous:
-        sound_manager.play_by_name("impulse", play_once=True)
-        landing = {"p": grounding["position"], "n": 5}
-        landed_previous = True
-    else:
-        landed_previous = False
-
-    if landing["n"] > 0:
-        landing["n"] -= 1
-
-    return landed_previous, landing
-def move_platform_body(body, platform_path, platform_path_index):
-    # Move the moving platform
-    destination = platform_path[platform_path_index]
-    current = Vec2d(*body.position)
-    distance = current.get_distance(destination)
-    if distance < PLATFORM_SPEED:
-        platform_path_index += 1
-        platform_path_index = platform_path_index % len(platform_path)
-        t = 1
-    else:
-        t = PLATFORM_SPEED / distance
-    new = current.interpolate_to(destination, t)
-
-    body.position = new
-    body.velocity = (new - current) / dt
-
-    return platform_path_index
-width, height = 690, 400
-fps = 60
-dt = 1.0 / fps
-PLAYER_VELOCITY = 100.0 * 2.0
-PLAYER_GROUND_ACCEL_TIME = 0.05
-PLAYER_GROUND_ACCEL = PLAYER_VELOCITY / PLAYER_GROUND_ACCEL_TIME
-
-PLAYER_AIR_ACCEL_TIME = 0.25
-PLAYER_AIR_ACCEL = PLAYER_VELOCITY / PLAYER_AIR_ACCEL_TIME
-
-JUMP_HEIGHT = 16.0 * 3
-JUMP_BOOST_HEIGHT = 24.0
-JUMP_CUTOFF_VELOCITY = 100
-FALL_VELOCITY = 250.0
-
-JUMP_LENIENCY = 0.05
-
-HEAD_FRICTION = 0.7
-
-PLATFORM_SPEED = 1
+from game_manager.src.sprite_sheet_array import PygameImageArray, AnimArray, FrameManager
+from game_settings import *
 
 girl_sprite = PygameImageArray(sprite_sheet_path='xmasgirl1.png',sprite_sheet_shape=(4, 4))
 
@@ -133,39 +50,14 @@ def blit_info(screen, font, clock):
             1,
             pygame.Color("darkgrey"),
         ),
-        (5, height - 35),
+        (5, HEIGHT - 35),
     )
     screen.blit(
         font.render("Press ESC or Q to quit", 1, pygame.Color("darkgrey")),
-        (5, height - 20),
+        (5, HEIGHT - 20),
     )
-def update_grounding(body, grounding):
-    """
-    Update the grounding information for the player.
-    
-    Args:
-        body: The player's body object.
-        grounding: A dictionary containing information about the player's grounding.
-    """
 
-    def f(arbiter):
-        n = -arbiter.contact_point_set.normal
-        if n.y > grounding["normal"].y:
-            grounding["normal"] = n
-            grounding["penetration"] = -arbiter.contact_point_set.points[0].distance
-            grounding["body"] = arbiter.shapes[1].body
-            grounding["impulse"] = arbiter.total_impulse
-            grounding["position"] = arbiter.contact_point_set.points[0].point_b
 
-    grounding.update({
-        "normal": Vec2d.zero(),
-        "penetration": Vec2d.zero(),
-        "impulse": Vec2d.zero(),
-        "position": Vec2d.zero(),
-        "body": None,
-    })
-    
-    body.each_arbiter(f)
 def helper_lines(screen):
     for y in [50, 100, 150, 200, 250, 300]:
         color = pygame.Color("green")
@@ -179,53 +71,46 @@ def init_physics(screen):
     draw_options = pymunk.pygame_util.DrawOptions(screen)
     return space, draw_options
 
-
-def add_segments(segments, space):
-    static = []
-    for segment in segments:
-        static.append(pymunk.Segment(space.static_body, segment["p1"], segment["p2"], segment["width"]))
-    return static
-
-box_walls = [
-{"p1": (10, 50), "p2": (300, 50), "width": 3},
-{"p1": (300, 50), "p2": (325, 50), "width": 3},
-{"p1": (325, 50), "p2": (350, 50), "width": 3},
-{"p1": (350, 50), "p2": (375, 50), "width": 3},
-{"p1": (375, 50), "p2": (680, 50), "width": 3},
-{"p1": (680, 50), "p2": (680, 370), "width": 3},
-{"p1": (680, 370), "p2": (10, 370), "width": 3},
-{"p1": (10, 370), "p2": (10, 50), "width": 3},
-]
-
-
-rounded_segments = [
-{"p1": (500, 50), "p2": (520, 60), "width": 3},
-{"p1": (520, 60), "p2": (540, 80), "width": 3},
-{"p1": (540, 80), "p2": (550, 100), "width": 3},
-{"p1": (350, 50), "p2": (550, 150), "width": 3},
-]
-
-platform_segments = [
-{"p1": (170, 50), "p2": (270, 150), "width": 3},
-{"p1": (270, 100), "p2": (300, 100), "width": 3},
-{"p1": (400, 150), "p2": (450, 150), "width": 3},
-{"p1": (400, 200), "p2": (450, 200), "width": 3},
-{"p1": (220, 200), "p2": (300, 200), "width": 3},
-{"p1": (50, 250), "p2": (200, 250), "width": 3},
-{"p1": (10, 370), "p2": (50, 250), "width": 3},
-]
-
 def save_screen(screen, filename="platformer.png"):
     pygame.image.save(screen, filename)
+
+def handle_jump(space, event, body, ground_velocity, remaining_jumps, well_grounded):
+    if well_grounded or remaining_jumps > 0:
+        jump_v = math.sqrt(2.0 * JUMP_HEIGHT * abs(space.gravity.y))
+        impulse = (0, body.mass * (ground_velocity.y + jump_v))
+        body.apply_impulse_at_local_point(impulse)
+        remaining_jumps -= 1
+
+def create_player(space):
+    player_dict = {}
+    body = pymunk.Body(5, float("inf"))
+    body.position = 100, 100
+
+    head = pymunk.Circle(body, 10, (0, 5))
+    head2 = pymunk.Circle(body, 10, (0, 13))
+    feet = pymunk.Circle(body, 10, (0, -5))
+    
+    mask = pymunk.ShapeFilter.ALL_MASKS() ^ pymunk.ShapeFilter(categories=0b1000).categories
+    sf = pymunk.ShapeFilter(mask=mask)
+    head.filter = sf
+    head2.filter = sf
+    feet.collision_type = 1
+    feet.ignore_draw = head.ignore_draw = head2.ignore_draw = True
+
+    space.add(body, head, feet, head2)
+    player_dict["body"] = body
+    player_dict["feet"] = feet
+    player_dict["head"] = head
+    return player_dict
+
+pygame.init()
 
 sound_manager = sound.SoundManager()
 sound_manager.add_sound_from_path("impulse", os.path.join(os.path.dirname(os.path.abspath(__file__)), "sfx.wav"))
 
-
 def main():
     ### PyGame init
-    pygame.init()
-    screen = pygame.display.set_mode((width, height))
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
     clock = pygame.time.Clock()
     running = True
@@ -265,68 +150,27 @@ def main():
 
     space.add(platform_body, s)
 
-    # pass through platform
-    passthrough = pymunk.Segment(space.static_body, (270, 100), (320, 100), 5)
-    passthrough.color = pygame.Color("yellow")
-    passthrough.friction = 1.0
-    passthrough.collision_type = 2
-    passthrough.filter = pymunk.ShapeFilter(categories=0b1000)
-    space.add(passthrough)
+    # Create the passthrough platform using the separate function
+    create_passthrough_platform(space)
 
-    def passthrough_handler(arbiter, space, data):
-        if arbiter.shapes[0].body.velocity.y < 0:
-            return True
-        else:
-            return False
-
-    space.add_collision_handler(1, 2).begin = passthrough_handler
-
-    # player
-    body = pymunk.Body(5, float("inf"))
-    body.position = 100, 100
-
-    head = pymunk.Circle(body, 10, (0, 5))
-    head2 = pymunk.Circle(body, 10, (0, 13))
-    feet = pymunk.Circle(body, 10, (0, -5))
-    
-    mask = pymunk.ShapeFilter.ALL_MASKS() ^ passthrough.filter.categories
-    sf = pymunk.ShapeFilter(mask=mask)
-    head.filter = sf
-    head2.filter = sf
-    feet.collision_type = 1
-    feet.ignore_draw = head.ignore_draw = head2.ignore_draw = True
-
-    space.add(body, head, feet, head2)
+    player_dict = create_player(space)
 
     remaining_jumps = 2
     landing = {"p": Vec2d.zero(), "n": 0}
-    frame_number = 0
 
     landed_previous = False
 
+    grounding = Grounding()
+
     while running:
-        grounding = {
-            "normal": Vec2d.zero(),
-            "penetration": Vec2d.zero(),
-            "impulse": Vec2d.zero(),
-            "position": Vec2d.zero(),
-            "body": None,
-        }
-
         # Update grounding
-        update_grounding(body, grounding)
+        grounding.update(player_dict["body"], player_dict["feet"])
 
-        well_grounded = False
-        if (
-            grounding["body"] is not None
-            and abs(grounding["normal"].x / grounding["normal"].y) < feet.friction
-        ):
-            well_grounded = True
+        well_grounded = grounding.is_well_grounded(player_dict["feet"].friction)
+        if well_grounded:
             remaining_jumps = 2
 
-        ground_velocity = Vec2d.zero()
-        if well_grounded:
-            ground_velocity = grounding["body"].velocity
+        ground_velocity = grounding.ground_velocity()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -335,17 +179,13 @@ def main():
                 save_screen(screen, "platformer.png")
 
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
-                if well_grounded or remaining_jumps > 0:
-                    jump_v = math.sqrt(2.0 * JUMP_HEIGHT * abs(space.gravity.y))
-                    impulse = (0, body.mass * (ground_velocity.y + jump_v))
-                    body.apply_impulse_at_local_point(impulse)
-                    remaining_jumps -= 1
+                handle_jump(space, event, player_dict["body"], ground_velocity, remaining_jumps, well_grounded)
             elif event.type == pygame.KEYUP and event.key == pygame.K_UP:
-                sprite_body.velocity = body.velocity.x, min(
-                    body.velocity.y, JUMP_CUTOFF_VELOCITY
+                sprite_body.velocity = player_dict["body"].velocity.x, min(
+                    player_dict["body"].velocity.y, JUMP_CUTOFF_VELOCITY
                 )
-                body.velocity = body.velocity.x, min(
-                    body.velocity.y, JUMP_CUTOFF_VELOCITY
+                player_dict["body"].velocity = player_dict["body"].velocity.x, min(
+                    player_dict["body"].velocity.y, JUMP_CUTOFF_VELOCITY
                 )
 
         # Target horizontal velocity of player
@@ -361,34 +201,34 @@ def main():
         if keys[pygame.K_DOWN]:
             frame_gen.add_anim_state("D")
 
-        feet.surface_velocity = -target_vx, 0
+        player_dict["feet"].surface_velocity = -target_vx, 0
 
-        if grounding["body"] is not None:
-            feet.friction = -PLAYER_GROUND_ACCEL / space.gravity.y
-            head.friction = HEAD_FRICTION
+        if grounding.body is not None:
+            player_dict["feet"].friction = -PLAYER_GROUND_ACCEL / space.gravity.y
+            player_dict["head"].friction = HEAD_FRICTION
         else:
-            feet.friction, head.friction = 0, 0
+            player_dict["feet"].friction, player_dict["head"].friction = 0, 0
 
         # Air control
-        if grounding["body"] is None:
-            body.velocity = Vec2d(
+        if grounding.body is None:
+            player_dict["body"].velocity = Vec2d(
                 cpflerpconst(
-                    body.velocity.x,
+                    player_dict["body"].velocity.x,
                     target_vx + ground_velocity.x,
                     PLAYER_AIR_ACCEL * dt,
                 ),
-                body.velocity.y,
+                player_dict["body"].velocity.y,
             )
 
-        body.velocity = body.velocity.x, max(
-            body.velocity.y, -FALL_VELOCITY
+        player_dict["body"].velocity = player_dict["body"].velocity.x, max(
+            player_dict["body"].velocity.y, -FALL_VELOCITY
         )  # clamp upwards as well?
 
         # Handle landing impulse
-        landed_previous, landing = handle_impulse(body, grounding, landed_previous, landing, sound_manager)
+        landed_previous, landing = handle_impulse(player_dict["body"], grounding, landed_previous, landing, sound_manager)
 
         # Move the moving platform
-        platform_path_index = move_platform_body(platform_body, platform_path, platform_path_index)
+        platform_path_index = move_platform_body(platform_body, platform_path, platform_path_index, PLATFORM_SPEED, dt)
 
         # ### Clear screen
         screen.fill(pygame.Color("black"))
@@ -398,18 +238,13 @@ def main():
         
         space.debug_draw(draw_options)
 
-        position = body.position + (-16, 28)
-        p = pymunk.pygame_util.to_pygame(position, screen)
-
-        angle = np.rad2deg(sprite_body.angle)
-
-        screen_height = screen.get_height()
-
-        pos = (sprite_body.position.x, screen_height - sprite_body.position.y)
-        blitRotate(screen, img_sprite, pos, (0, image_size[1]), angle)
+        blitRotate(screen, sprite_body, img_sprite, (0, image_size[1]))
 
         img = frame_gen.get_frame()
-        mask = pygame.mask.from_surface(img)
+        
+        position = player_dict["body"].position + (-16, 28)
+        p = pymunk.pygame_util.to_pygame(position, screen)
+
         screen.blit(img, p)
 
         if landing["n"] > 0:
@@ -418,13 +253,11 @@ def main():
 
         blit_info(screen, font, clock)
         pygame.display.flip()
-        frame_number += 1
 
         ### Update physics
-
         space.step(dt)
 
-        clock.tick(fps)
+        clock.tick( )
 
 if __name__ == "__main__":
     sys.exit(main())
